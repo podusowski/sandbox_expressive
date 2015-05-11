055baa4e4b2105a73d76429ef74e6f22df39e243
Piotr Podusowski <podusowski@gmail.com>
2015-05-11 21:37:23 +0200

    misc

--- composition.cpp
+++ composition.cpp
@@ -31,7 +31,9 @@ auto bar(strict_type<2> value) -> strict_type<3>
 
 int main()
 {
-    auto composed = compose(&foo, &bar);
-    strict_type<3> ret = composed(strict_type<1>{3});
-    assert(3 == *ret);
+    {
+        auto composed = compose(&foo, &bar);
+        strict_type<3> ret = composed(strict_type<1>{3});
+        assert(3 == *ret);
+    }
 }
