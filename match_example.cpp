22799864b32c913c981f7ba8a4b8de334408876e
Piotr Podusowski <podusowski@gmail.com>
2015-05-15 13:20:03 +0200

    formatting

--- match_example.cpp
+++ match_example.cpp
@@ -12,8 +12,8 @@ int main()
     auto i = 4;
 
     functional::match(i)
-                     (2 ,[]{ std::cout << "it's 2!" << std::endl; })
-                     (4 ,[]{ std::cout << "it's 4!" << std::endl; })
+                     (2 ,      []{ std::cout << "it's 2!" << std::endl; })
+                     (4 ,      []{ std::cout << "it's 4!" << std::endl; })
                      (is_even, []{ std::cout << "it's even!" << std::endl; });
 
 //    functional::match(i,       2)
