c6f1597739017d84572660da3a7602f8cccb9c5b
Piotr Podusowski <podusowski@gmail.com>
2015-05-18 16:25:25 +0200

    strong_type

---
+++ examples/strong_type_example.cpp
@@ -0,0 +1,27 @@
+#include "unpure/strong_type.hpp"
+
+#include <type_traits>
+#include <iostream>
+
+struct player_id_tag {};
+struct ship_id_tag {};
+
+
+int main()
+{
+    using player_id_type = unpure::strong_type<player_id_tag, int>;
+    using ship_id_type = unpure::strong_type<ship_id_tag, int>;
+
+    auto player_id = player_id_type{1};
+    auto ship_id = ship_id_type{1};
+
+    // calling void foo(player_id_type id); function with ship_id will not compile
+
+    std::cout << player_id << std::endl;
+
+    static_assert(not std::is_convertible<decltype(player_id), decltype(ship_id)>::value, "two strong_type<>'s should not be convertible");
+
+    auto another_player_id = unpure::strong_int<player_id_tag>{2};
+
+    static_assert(std::is_same<decltype(another_player_id), decltype(player_id)>::value, "string_int should be string_type<int>");
+}
---
+++ expressive/unpure/strong_type.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <ostream>
+#include <typeinfo>
+
+namespace unpure
+{
+
+template<class Tag, class Underlaying>
+struct strong_type
+{
+    explicit strong_type(Underlaying value) : value(value)
+    {
+    }
+
+    auto operator = (Underlaying value) -> strong_type &
+    {
+        this->value = value;
+        return *this;
+    }
+
+    auto operator == (strong_type<Tag, Underlaying> other) const -> bool
+    {
+        return value == other.value;
+    }
+
+    auto get() const -> Underlaying
+    {
+        return value;
+    }
+
+private:
+    Underlaying value;
+};
+
+template<class Tag>
+using strong_int = strong_type<Tag, int>;
+
+template<class Tag>
+using strong_unsigned = strong_type<Tag, unsigned>;
+
+template<class Tag, class Underlaying>
+std::ostream & operator << (std::ostream & os, strong_type<Tag, Underlaying> val)
+{
+    return os << typeid(Tag).name() << ":" << val.get();
+}
+
+} // namespace unpure
