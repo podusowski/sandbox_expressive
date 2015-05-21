2ac6670e9e2c97b650ccc1fd5f96828fff36acd9
Piotr Podusowski <podusowski@gmail.com>
2015-05-21 10:21:46 +0200

    factory basics

--- examples/factory.cpp
+++ examples/factory.cpp
@@ -1,11 +1,20 @@
 #include "factory.hpp"
 
+#include <iostream>
+#include <cassert>
+
 struct interface
 {
+    virtual auto say_hello() -> void = 0;
+    virtual ~interface() {}
 };
 
 struct concrete : public interface
 {
+    auto say_hello() -> void override
+    {
+        std::cout << "hello from concrete struct!" << std::endl;
+    }
 };
 
 using interface_factory = expressive::factory<interface>;
@@ -13,6 +22,9 @@ using interface_factory = expressive::factory<interface>;
 void foo(interface_factory & factory)
 {
     auto object = factory.create();
+
+    assert(object);
+    object->say_hello();
 }
 
 int main()
--- expressive/factory.hpp
+++ expressive/factory.hpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <functional>
 
 namespace expressive
 {
@@ -7,17 +8,25 @@ template<class Base>
 struct factory
 {
     using pointer = std::shared_ptr<Base>;
+    using impl_type = std::function<pointer()>;
 
     auto create() -> pointer
     {
-        return pointer{};
+        return impl();
     }
 
     template<class Concrete>
     static auto bind() -> factory<Base>
     {
-        return factory<Base>{};
+        return factory<Base>{std::bind(std::make_shared<Concrete>)};
     }
+
+private:
+    explicit factory(impl_type impl) : impl(impl)
+    {
+    }
+
+    impl_type impl;
 };
 
 } // namespace expressive
