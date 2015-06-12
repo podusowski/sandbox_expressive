978a86dd3030b486e3b42914cb03b738e8c2cc7f
Piotr Podusowski <podusowski@gmail.com>
2015-06-12 10:55:39 +0200

    optimize

--- expressive/container_cast.hpp
+++ expressive/container_cast.hpp
@@ -4,7 +4,7 @@ namespace expressive
 {
 
 template<class to_type, class from_type>
-auto container_cast(from_type container)
+auto container_cast(from_type && container)
 {
     return to_type{container.begin(), container.end()};
 }
