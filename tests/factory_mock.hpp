f9caa26b851ca78051bc94df5e27014493adff65
Piotr Podusowski <podusowski@gmail.com>
2015-06-17 11:18:44 +0200

    misc

--- tests/factory_mock.hpp
+++ tests/factory_mock.hpp
@@ -4,8 +4,8 @@
 
 #include "factory.hpp"
 
-template<class Base, class Arg0, class Arg1>
-struct factory_mock : public expressive::factory<Base, Arg0, Arg1>
+template<class Base, class... Args>
+struct factory_mock : public expressive::factory<Base, Args...>
 {
     MOCK_CONST_METHOD1_T(create, std::shared_ptr<Base>(Arg0));
     MOCK_CONST_METHOD2_T(create, std::shared_ptr<Base>(Arg0, Arg1));
