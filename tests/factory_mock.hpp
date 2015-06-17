5edbe7cb37420ed866befe5357a9599c0ad3454c
Piotr Podusowski <podusowski@gmail.com>
2015-06-17 12:27:26 +0200

    working factory_mock

--- tests/factory_mock.hpp
+++ tests/factory_mock.hpp
@@ -3,11 +3,21 @@
 #include <gmock/gmock.h>
 
 #include "factory.hpp"
+#include "expressive/nth_element_or.hpp"
+
+namespace expressive
+{
 
 template<class Base, class... Args>
 struct factory_mock : public expressive::factory<Base, Args...>
 {
-    MOCK_CONST_METHOD1_T(create, std::shared_ptr<Base>(Arg0));
-    MOCK_CONST_METHOD2_T(create, std::shared_ptr<Base>(Arg0, Arg1));
-    //MOCK_METHOD3_T(create, std::shared_ptr<Base>(Arg0, Arg1, Arg2));
+    template<unsigned index>
+    using arg = typename nth_element_or<index, int, Args...>::type;
+
+    MOCK_CONST_METHOD1_T(create, std::shared_ptr<Base>(arg<0>));
+    MOCK_CONST_METHOD2_T(create, std::shared_ptr<Base>(arg<0>, arg<1>));
+    MOCK_CONST_METHOD3_T(create, std::shared_ptr<Base>(arg<0>, arg<1>, arg<2>));
+    MOCK_CONST_METHOD4_T(create, std::shared_ptr<Base>(arg<0>, arg<1>, arg<2>, arg<3>));
 };
+
+} // namespace expressive
