040d557c2e946532c73060010c6c192d7f688222
Piotr Podusowski <podusowski@gmail.com>
2015-06-30 15:27:58 +0200

    map_element

---
+++ examples/map_element.cpp
@@ -0,0 +1,33 @@
+#include <map>
+#include <iostream>
+
+namespace expressive
+{
+
+template<class Map, class Action>
+auto map_element(const Map & map, typename Map::key_type key, Action action)
+{
+    const auto it = map.find(key);
+
+    if (it != map.end())
+    {
+        action(it->second);
+    }
+}
+
+} // namespace expressive
+
+auto main() -> int
+{
+    const auto map = std::map<int, std::string>{{1, "one"}, {2, "two"}};
+
+    expressive::map_element(map, 1, [](auto s)
+    {
+        std::cout << s << std::endl;
+    });
+
+    expressive::map_element(map, 10, [](auto s)
+    {
+        std::cout << "I shouldn't be called" << std::endl;
+    });
+}
