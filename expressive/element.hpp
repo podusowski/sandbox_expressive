211b07e2cb5dfdc4cc91acc5e0050a9961f2bca4
Piotr Podusowski <podusowski@gmail.com>
2015-06-30 16:02:50 +0200

    else block in element

--- expressive/element.hpp
+++ expressive/element.hpp
@@ -14,4 +14,19 @@ auto element(const Map & map, typename Map::key_type key, Action action)
     }
 }
 
+template<class Map, class Action, class Else>
+auto element(const Map & map, typename Map::key_type key, Action action, Else els)
+{
+    const auto it = map.find(key);
+
+    if (it != map.end())
+    {
+        action(it->second);
+    }
+    else
+    {
+        els();
+    }
+}
+
 } // namespace expressive
--- tests/element_tests.cpp
+++ tests/element_tests.cpp
@@ -16,14 +16,41 @@ struct action_mock
     MOCK_METHOD1_T(call, void(T));
 };
 
+template<class T>
+using action_strict_mock = testing::StrictMock<action_mock<T>>;
+
+struct else_mock
+{
+    MOCK_METHOD0(call, void());
+};
+
+using else_strict_mock = testing::StrictMock<else_mock>;
+
 TEST(element_tests, action_called_when_element_is_found_on_const_map)
 {
-    action_mock<std::string> action;
+    action_strict_mock<std::string> action;
 
     EXPECT_CALL(action, call("one"));
 
     expressive::element(map_of_numbers, 1, [&](auto s)
     {
         action.call(s);
+    });
+}
+
+TEST(element_tests, else_action_called_when_element_is_missing)
+{
+    action_strict_mock<std::string> action;
+    else_mock els;
+
+    EXPECT_CALL(els, call());
+
+    expressive::element(map_of_numbers, 10, [&](auto s)
+    {
+        action.call(s);
+    },
+    [&]()
+    {
+        els.call();
     });
 }
