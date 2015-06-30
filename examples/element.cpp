79c2f7891dc66e9a1588c46b2f949cc051a64d2f
Piotr Podusowski <podusowski@gmail.com>
2015-06-30 15:42:55 +0200

    map_element renamed to element

--- examples/element.cpp
+++ examples/element.cpp
@@ -5,7 +5,7 @@ namespace expressive
 {
 
 template<class Map, class Action>
-auto map_element(const Map & map, typename Map::key_type key, Action action)
+auto element(const Map & map, typename Map::key_type key, Action action)
 {
     const auto it = map.find(key);
 
@@ -21,12 +21,12 @@ auto main() -> int
 {
     const auto map = std::map<int, std::string>{{1, "one"}, {2, "two"}};
 
-    expressive::map_element(map, 1, [](auto s)
+    expressive::element(map, 1, [](auto s)
     {
         std::cout << s << std::endl;
     });
 
-    expressive::map_element(map, 10, [](auto s)
+    expressive::element(map, 10, [](auto s)
     {
         std::cout << "I shouldn't be called" << std::endl;
     });
