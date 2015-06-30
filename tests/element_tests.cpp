e8e218c366665eebf9a763728baa03ef01592571
Piotr Podusowski <podusowski@gmail.com>
2015-06-30 15:56:32 +0200

    some basic test for element

--- tests/element_tests.cpp
+++ tests/element_tests.cpp
@@ -10,9 +10,20 @@ const auto map_of_numbers = std::map<int, std::string>{{1, "one"}, {2, "two"}};
 
 } // namespace
 
+template<class T>
+struct action_mock
+{
+    MOCK_METHOD1_T(call, void(T));
+};
+
 TEST(element_tests, action_called_when_element_is_found_on_const_map)
 {
-    expressive::element(map_of_numbers, 1, [](auto s)
+    action_mock<std::string> action;
+
+    EXPECT_CALL(action, call("one"));
+
+    expressive::element(map_of_numbers, 1, [&](auto s)
     {
+        action.call(s);
     });
 }
