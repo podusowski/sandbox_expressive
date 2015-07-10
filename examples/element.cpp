9929b58621539f64fb6138259362748324e92913
Piotr Podusowski <podusowski@gmail.com>
2015-07-10 15:16:50 +0200

    misc

--- examples/element.cpp
+++ examples/element.cpp
@@ -3,9 +3,14 @@
 
 #include "expressive/element.hpp"
 
+auto make_map()
+{
+    return std::map<int, std::string>{{1, "one"}, {2, "two"}};
+}
+
 auto element_exists()
 {
-    const auto map = std::map<int, std::string>{{1, "one"}, {2, "two"}};
+    const auto map = make_map();
 
     const auto found = expressive::element(map, 1, [](auto s)
     {
@@ -17,7 +22,7 @@ auto element_exists()
 
 auto element_missing()
 {
-    const auto map = std::map<int, std::string>{{1, "one"}, {2, "two"}};
+    const auto map = make_map();
 
     const auto found = expressive::element(map, 10, [](auto s)
     {
