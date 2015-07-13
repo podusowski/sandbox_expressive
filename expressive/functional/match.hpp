663eff05d182b17e99eb2f112459b265bb492283
Piotr Podusowski <podusowski@gmail.com>
2015-07-13 20:39:34 +0200

    namespace

--- expressive/functional/match.hpp
+++ expressive/functional/match.hpp
@@ -14,6 +14,9 @@ struct bad_match : public std::runtime_error
     }
 };
 
+namespace match_details
+{
+
 template<class value_type>
 auto check(const value_type & value, const value_type & equal_to) -> bool
 {
@@ -53,6 +56,8 @@ auto match_impl(const value_type & value,
     }
 }
 
+} // namespace match_details
+
 template<class value_type,
          class value_or_preficate_type,
          class action_type,
@@ -62,7 +67,7 @@ auto match(const value_type & value,
            other_types... others)
 {
     using return_type = decltype(expressive::call_with_one_arg_or_none(action, value));
-    return match_impl<return_type>(value, match_with, action, others...);
+    return match_details::match_impl<return_type>(value, match_with, action, others...);
 }
 
 struct otherwise_t
