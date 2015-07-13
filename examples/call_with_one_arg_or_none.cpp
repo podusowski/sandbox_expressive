1f9e5fab09a84b4648f4dd8e7d90bae779760ef1
Piotr Podusowski <podusowski@gmail.com>
2015-07-13 20:24:45 +0200

    call_wt... returns a value

--- examples/call_with_one_arg_or_none.cpp
+++ examples/call_with_one_arg_or_none.cpp
@@ -4,7 +4,9 @@
 
 auto main() -> int
 {
-    expressive::call_with_one_arg_or_none([](int x) { std::cout << "have arg: " << x << std::endl; }, 10);
+    int i = expressive::call_with_one_arg_or_none([](int x) { std::cout << "have arg: " << x << std::endl; return 5; }, 10);
+    std::cout << i << std::endl;
+
     expressive::call_with_one_arg_or_none([](auto x) { std::cout << "have arg: " << x << std::endl; }, 10);
     expressive::call_with_one_arg_or_none([]()      { std::cout << "no arg" << std::endl; }, 10);
 }
--- expressive/meta/call_with_one_arg_or_none.hpp
+++ expressive/meta/call_with_one_arg_or_none.hpp
@@ -8,13 +8,13 @@ namespace expressive
 template<class function_type, class arg_type>
 auto call_with_one_arg_or_none(function_type function, arg_type arg) -> decltype(function(std::declval<arg_type>()))
 {
-    function(arg);
+    return function(arg);
 }
 
 template<class function_type, class arg_type>
 auto call_with_one_arg_or_none(function_type function, arg_type arg) -> decltype(function())
 {
-    function();
+    return function();
 }
 
 } // namespace expressive
