72715899029d8c10c808f41581971cd30910346c
Piotr Podusowski <podusowski@gmail.com>
2015-07-13 20:23:20 +0200

    call with arg now works with templated funtor

--- expressive/meta/call_with_one_arg_or_none.hpp
+++ expressive/meta/call_with_one_arg_or_none.hpp
@@ -1,20 +1,18 @@
 #pragma once
 
-#include "expressive/meta/function_traits.hpp"
-
 #include <type_traits>
 
 namespace expressive
 {
 
 template<class function_type, class arg_type>
-auto call_with_one_arg_or_none(function_type function, arg_type arg) -> typename std::enable_if<function_traits<function_type>::has_arg, void>::type
+auto call_with_one_arg_or_none(function_type function, arg_type arg) -> decltype(function(std::declval<arg_type>()))
 {
     function(arg);
 }
 
 template<class function_type, class arg_type>
-auto call_with_one_arg_or_none(function_type function, arg_type arg) -> typename std::enable_if<!function_traits<function_type>::has_arg, void>::type
+auto call_with_one_arg_or_none(function_type function, arg_type arg) -> decltype(function())
 {
     function();
 }
