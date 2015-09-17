65342e51862fc999487f51d891c212519721f9d4
Piotr Podusowski <podusowski@gmail.com>
2015-09-17 14:46:34 +0200

    refactor

--- expressive/make_string.hpp
+++ expressive/make_string.hpp
@@ -6,14 +6,14 @@ namespace make_string_detail
 {
 
 template<class T>
-auto print_to(T && value, std::ostream & os, int) -> typename std::enable_if<std::is_same<decltype(os << value),
+auto print_value_or_container_impl(T && value, std::ostream & os, int) -> typename std::enable_if<std::is_same<decltype(os << value),
                                                                                           std::ostream &>::value>::type
 {
     os << value;
 }
 
 template<class T>
-auto print_to(T && container, std::ostream & os, float) -> typename std::enable_if<std::is_same<decltype(container.begin()),
+auto print_value_or_container_impl(T && container, std::ostream & os, float) -> typename std::enable_if<std::is_same<decltype(container.begin()),
                                                                                                 decltype(container.end())>::value>::type
 {
     auto it = container.begin();
@@ -29,13 +29,19 @@ auto print_to(T && container, std::ostream & os, float) -> typename std::enable_
     }
 }
 
+template<class T>
+auto print_value_or_container(T && value, std::ostream & os)
+{
+    print_value_or_container_impl(value, os, 0);
+}
+
 } // namespace make_string_detail
 
 template<class T>
 auto make_string(T && p_param)
 {
     std::stringstream l_ss;
-    make_string_detail::print_to(p_param, l_ss, 0);
+    make_string_detail::print_value_or_container(p_param, l_ss);
     return l_ss.str();
 }
 
@@ -43,7 +49,7 @@ template<class T, class... U>
 auto make_string(T && p_param, U &&... p_params)
 {
     std::stringstream l_ss;
-    make_string_detail::print_to(p_param, l_ss, 0);
-    make_string_detail::print_to(make_string(std::forward<U>(p_params)...), l_ss, 0);
+    make_string_detail::print_value_or_container(p_param, l_ss);
+    make_string_detail::print_value_or_container(make_string(std::forward<U>(p_params)...), l_ss);
     return l_ss.str();
 }
